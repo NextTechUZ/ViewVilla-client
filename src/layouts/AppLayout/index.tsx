63b0d9de08b151e04c5291ad14562dfa248f1d9d
FazliddinMirzaqosimov <fazliddinmirzaqosimov8@gmail.com>
2023-06-10 16:36:59 +0500

    bug fixed

--- src/layouts/AppLayout/index.tsx
+++ src/layouts/AppLayout/index.tsx
@@ -1,21 +1,29 @@
-import React from 'react'
-import { MainPropTypes } from '../../shared/types'
-import Navbar from '../Navbar'
-import Footer from '../Footer'
-import { ToastContainer } from 'react-toastify'
+import React, { useEffect, useState } from "react";
+import { MainPropTypes } from "../../shared/types";
+import Navbar from "../Navbar";
+import Footer from "../Footer";
+import { ToastContainer } from "react-toastify";
 
+function AppLayout({ children }: MainPropTypes) {
+  const [isActive, setIsActive] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setIsActive(false);
+    }, 5000);
+  }, []);
 
-
-function AppLayout({children}:MainPropTypes ) {
-  return (
-    <div  >
-        <Navbar/>
-        {children}
-        <Footer/>
-        <ToastContainer />
-
-        </div>
-  )
+  return isActive ? (
+    <video autoPlay muted playsInline style={{width:'100vw',height:"100vh",objectFit:"contain"}}>
+      <source src="/media/logo.mp4" type="video/mp4" />
+    </video>
+  ) : (
+    <div>
+      <Navbar />
+      {children}
+      <Footer />
+      <ToastContainer />
+    </div>
+  );
 }
 
-export default AppLayout
+export default AppLayout;

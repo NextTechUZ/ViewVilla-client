eac233bade49b549b6768ef7d6b0ef2b964d5fb1
FazliddinMirzaqosimov <fazliddinmirzaqosimov8@gmail.com>
2023-06-28 19:09:18 +0500

    bug fixed

--- src/SEO/index.tsx
+++ src/SEO/index.tsx
@@ -16,9 +16,16 @@ function SEO() {
       <meta
         name="keywords"
         content="viewvilla vewvilla view-villa viewvillauz viewvilla.uz dacha dachauz dacha.uz sijje sijjak tog villa  luxury villas, vacation homes, villa rentals, exquisite villas, luxury vacation rentals, villa accommodations, premium villas, luxurious retreats, stunning views, top-notch amenities, dream vacations"
-      />https://www.viewvilla.uz/
+      />
 
-      
+      <meta property="og:title" content="ViewVilla" />
+      <meta
+        property="og:description"
+        content="Bizning ajoyib dachalarimzda hashamatli dam oling. Yuqori darajadagi qulayliklari va hayratlanarli manzaralari bilan bizning ajoyib dachamizni ijaraga oling. Hoziroq orzuingizdagi dachani bron qiling!"
+      />
+      <meta property="og:image" content={image.src} />
+      <meta property="og:url" content="https://www.viewvilla.uz/" />
+      <meta property="og:type" content="website" />
     </Head>
   );
 }
